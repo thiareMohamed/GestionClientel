ca121f520e93cf5179fb966d3ce1fe3d0170165d
ibsonngit <thiaremohamed.mt@gmail.com>
2022-07-14 07:57:13 +0000

    initial commit

---
+++ src/main/java/sn/thiare/gestionclientel/GestionClientelApplication.java
@@ -0,0 +1,13 @@
+package sn.thiare.gestionclientel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionClientelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionClientelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/thiare/gestionclientel/GestionClientelApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.thiare.gestionclientel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClientelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
